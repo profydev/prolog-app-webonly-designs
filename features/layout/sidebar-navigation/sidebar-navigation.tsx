fb697bf0ee7ee022f9eb7be53e573b8ac7b1eb83
Akshay Agarwal <webonly.designs@gmail.com>
2024-02-17 05:23:54 -0600

    fixed sidebar navigation arrow (#1)
    Fixed sidebar navigation arrow to right when collapsed.

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -87,10 +87,14 @@ export function SidebarNavigation() {
             />
             <MenuItemButton
               text="Collapse"
-              iconSrc="/icons/arrow-left.svg"
+              iconSrc={
+                isSidebarCollapsed
+                  ? "/icons/arrow-right.svg"
+                  : "/icons/arrow-left.svg"
+              }
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
-              className={styles.collapseMenuItem}
+              className={classNames(styles.collapseMenuItem)}
             />
           </ul>
         </nav>
